62492483143e02e7c9b1bf5dc5f2112ca5707b64
raven99p <paulinhoellul@gmail.com>
2023-12-17 02:44:39 +0200

    add gitignore and fix code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from svm import SVM
+import numpy as np
 
 df = pd.read_csv("./data.csv", delimiter=";", usecols=["dr", "p", "class"])
 
